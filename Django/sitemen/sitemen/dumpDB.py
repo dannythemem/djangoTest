3c9286b799f4e94debe2f0bcf905880cf006c452
dannythemem <82832653+dannythemem@users.noreply.github.com>
2025-07-16 15:17:59 +0300

    Добавил .gitignore, удаление дублирующейся папки проекта

--- Django/sitemen/sitemen/dumpDB.py
+++
@@ -1,8 +0,0 @@
-from django.core.management import call_command
-import os
-import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sitemen.settings')
-django.setup()
-with open('../../../fixtures/data.json', 'w', encoding='utf-8') as f:
-    call_command('dumpdata', 'men.Category', 'men.Men', 'men.Wife', 'men.TagPost', stdout=f)
